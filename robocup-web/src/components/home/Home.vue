8e794225dab0f6c9668ea399c356e0558a6fab09
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-07-03 00:05:21 +0900

    불필요 코드 제거

--- robocup-web/src/components/home/Home.vue
+++ robocup-web/src/components/home/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
     <v-layout text-xs-center wrap>
-      {{ userName }}
       <v-flex xs12>
         <v-img :src="require('../../assets/logo.svg')" class="my-3" contain height="200"></v-img>
       </v-flex>
@@ -14,27 +13,8 @@
 </template>
 
 <script>
-let displayName = "";
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    console.log(displayName);
-  } else {
-  }
-});
-
 export default {
-  data: () => ({}),
-  watch: {
-    userName: displayName
-  }
+  
 };
 </script>
 
--- robocup-web/src/components/login/Login.vue
+++ robocup-web/src/components/login/Login.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div id="firebaseui-auth-container"></div>
-    <div id="loader"></div>
   </div>
 </template>
 
