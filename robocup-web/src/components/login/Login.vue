66090c7ce694ef182ca2bc6e3620d684656d3d9f
KimYoonho Aaron <youknow.yoonho@gmail.com>
2019-07-05 00:07:32 +0900

    Login 화면 개선 및 플로우 개선

--- robocup-web/src/components/login/Login.vue
+++ robocup-web/src/components/login/Login.vue
@@ -1,9 +1,27 @@
 <template>
-  <v-container fill-height style="max-width: 450px">
-    <v-layout align-center row wrap>
-      <div id="firebaseui-auth-container"></div>
-    </v-layout>
-  </v-container>
+  <div>
+    <v-container
+      fluid
+      pa-0
+      style="max-width:100%;max-height:100%; height:400px; background-image: url('journalist.jpg'); background-size: cover; background-position: center center;"
+    >
+      <v-layout
+        pa-5
+        text-xs-center
+        style="background-color: #00000099; color:#FFFFFF; height:400px"
+      >
+        <v-flex>
+          <h1 class="font-weight-bold mt-5">Are you a journalist?</h1>
+          <h1 class="display-4 font-weight-bold mt-5">Join in!</h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container fill-height style="max-width: 450px">
+      <v-layout align-center row wrap>
+        <div id="firebaseui-auth-container"></div>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -11,6 +29,7 @@ export default {
   mounted() {
     // FirebaseUI config.
     const uiConfig = {
+      signInFlow: 'popup',
       signInSuccessUrl: "/",
       signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
       // Terms of service url/callback.
