7072b592b74050e0e4bb7e664d1f49e5eec4dd22
Yoonho Aaron Kim <yoonho.kim@hp.com>
2019-07-04 16:10:09 +0900

    /teams/add 기본 구현

--- robocup-web/src/components/teams/Teams.vue
+++ robocup-web/src/components/teams/Teams.vue
@@ -1,13 +1,40 @@
 <template>
+  <div>
     <div>Teams</div>
+    <v-btn v-show="isSignIn" fab dark large color="cyan" href="/teams/add">
+      <v-icon dark>add</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    isSignIn: false,
+    // teams: [],
+    // ref: firebase.firestore().collection("teams")
+  }),
+  created() {
+    this.isSignIn =
+      localStorage.getItem("firebaseui::rememberedAccounts") != null;
 
-}
+    // todo: Get team list
+    // this.ref.onSnapshot(querySnapshot => {
+    //   this.articles = [];
+    //   querySnapshot.forEach(doc => {
+    //     const article = doc.data();
+    //     this.articles.push({
+    //       id: article.id,
+    //       thumbnailUrl: getThumbnailUrl(article),
+    //       title: article.title,
+    //       subtitle: article.contents,
+    //       writer: article.writerName
+    //     });
+    //   });
+    // });
+  }
+};
 </script>
 
 <style>
-
 </style>
